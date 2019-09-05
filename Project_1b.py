1231d684f1f0b02cc8bebd69d3866461cfc239b6
George Cowie <gecocowie@live.com>
2019-09-05 14:58:19 +0200

    fgdgf

--- Project_1b.py
+++ Project_1b.py
@@ -5,10 +5,10 @@ def f(x):               # 3 flops
     return 100*np.exp(-10*x)
 
 # total 4 flops
-n = int(input("Size of the matrix (10, 100 or 1000): "))
-x = np.linspace(0,1,n)
-h = 1/(n+1)
-q = (h**2)*f(x)
+#n = int(input("Size of the matrix (10, 100 or 1000): "))
+#x = np.linspace(0,1,n)
+#h = 1/(n+1)
+#q = (h**2)*f(x)
 
 def array(n, name_of_array):
     arr = input("Consistent(c) or random(r) numbers on vector %s ?: " % (name_of_array))
@@ -27,40 +27,76 @@ def array(n, name_of_array):
             sys.exit("ERROR! You must insert a legal letter or integer.")
     return arr
 
-
-####   Initilize vectors   ####
-a = array(n, "upper diagonal")
-b = array(n, "diagonal")
-c = array(n, "lower diagonal")
-u = np.zeros(n)
-b_temp = np.zeros(n)
-q_temp = np.zeros(n)
-
-
-####    step 2 (forward substitution)   ####
-t0 = time.time()
-b_temp[0] = b[0]
-q_temp[0] = q[0]
-for i in range(1,n):
-    b_temp[i] = b[i] - a[i]*c[i-1]/b_temp[i-1]
-    q_temp[i] = q[i]-a[i]*q_temp[i-1]/b_temp[i-1]
+def gen_algo(n): #general algorithm
+    t0 = time.time()
+    ####   Initilize vectors   ####
+    #n = int(input("Size of the matrix (10, 100 or 1000): "))
+    x = np.linspace(0,1,n)
+    h = 1/(n+1)
+    q = (h**2)*f(x)
+    a = np.ones(n)*-1#array(n, "upper diagonal")
+    b = np.ones(n)*2#array(n, "diagonal")
+    c = np.ones(n)*-1#array(n, "lower diagonal")
+    u = np.zeros(n)
+    b_temp = np.zeros(n)
+    q_temp = np.zeros(n)
 
 
-####    step 3 (backward substitution)   ####
-u[-2] = q_temp[-1]/b_temp[-1]
-for i in range(2,n):          # 3*n flops
-    u[-i] = (q_temp[-i] - c[-i]*u[-i+1])/b_temp[-i]
+    ####    step 2 (forward substitution)   ####
+    t0 = time.time()
+    b_temp[0] = b[0]
+    q_temp[0] = q[0]
+    for i in range(1,n):
+        b_temp[i] = b[i] - a[i]*c[i-1]/b_temp[i-1]
+        q_temp[i] = q[i]-a[i]*q_temp[i-1]/b_temp[i-1]
 
+
+    ####    step 3 (backward substitution)   ####
+    u[-2] = q_temp[-1]/b_temp[-1]
+    for i in range(2,n):          # 3*n flops
+        u[-i] = (q_temp[-i] - c[-i]*u[-i+1])/b_temp[-i]
+    t1 = time.time()
+    total = t1-t0
+    print('CPU time: %.5g s' % total)
+    print('Grid size:',n)
+    return u,x
 
 def v(x):       # closed-formed solution, 7 flops
     return 1 - (1-np.exp(-10))*x - np.exp(-10*x)
 
-t1 = time.time()
-total = t1-t0
-print('CPU time:',total)
 
-plt.plot(x,u)
-plt.plot(x,v(x))
-plt.legend(["Numerical solution","Closed-form solution"])
-plt.title("Grid size = %i, CPU time = %.3f s" % (n,total)); plt.grid()
-plt.show()
+
+#### make plots #####
+u_10,x_10 = gen_algo(1000)
+u_100,x_100 = gen_algo(10000)
+u_1000,x_1000 = gen_algo(100000)
+u_1000,x_1000 = gen_algo(1000000)
+x_vec = np.linspace(0,1,1000)
+
+plt.plot(x_10,u_10,label = 'n=10')
+plt.plot(x_100,u_100,label = 'n=100')
+plt.plot(x_1000,u_1000,label = 'n=1000')
+
+plt.plot(x_vec,v(x_vec), label = 'Closed-form')
+plt.title("Closed-form/numerical comparison");
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.legend()
+plt.grid()
+#plt.show()
+
+
+"""
+program output 1b
+CPU time: 2e-03 s
+Grid size: 1000
+
+CPU time: 2e-02 s
+Grid size: 10000
+
+CPU time: 2e-01 s
+Grid size: 100000
+
+CPU time: 2e+00 s
+Grid size: 1000000
+"""
--- project_1c.py
+++ project_1c.py
@@ -4,12 +4,6 @@ import numpy as np, matplotlib.pyplot as plt, random as r, time
 def f(x):               # 3 flops
     return 100*np.exp(-10*x)
 
-# total 4 flops
-n = int(input("Size of the matrix (10, 100 or 1000): "))
-x = np.linspace(0,1,n)
-h = 1/(n-1)
-q = (h**2)*f(x)
-
 def array(n, name_of_array):
     arr = input("Consistent(c) or random(r) numbers on vector %s ?: " % (name_of_array))
     if arr == "c" or arr == "C":
@@ -33,34 +27,67 @@ b = 2 #np.ones(n)*float(input("Values of vector b (below diagonal): "))
 c = -1 #np.ones(n)*float(input("Values of vector c (over diagonal): "))
 #b_temp = np.zeros(n)
 
-index = np.linspace(1,n,n)
-b_temp = (index+1)/index
-q_temp = np.zeros(n)
-u = np.zeros(n)
-t0 = time.time()
+def spes_algo(n):
+    #n = int(input("Size of the matrix (10, 100 or 1000): "))
+    x = np.linspace(0,1,n)
+    h = 1/(n-1)
+    q = (h**2)*f(x)
+    t0 = time.time()
+    index = np.linspace(1,n,n)
+    b_temp = (index+1)/index
+    q_temp = np.zeros(n)
+    u = np.zeros(n)
+
+    ####    Forward sub   ####
+    #b_temp[0] = b
+    for i in range(1,n):
+        #b_temp[i] = b - 1/b_temp[i-1]
+        q_temp[i] = q[i]-q_temp[i-1]*a/b_temp[i-1]
 
 
-####    Forward sub   ####
-#b_temp[0] = b
-for i in range(1,n):
-    #b_temp[i] = b - 1/b_temp[i-1]
-    q_temp[i] = q[i]-q_temp[i-1]*a/b_temp[i-1]
-
-
-#### Backward ####
-u[-2] = q_temp[-1]/b_temp[-1]
-for i in range(2,n):          # 3*n flops
-    u[-i] = (q_temp[-i] - a*u[-i+1])/b_temp[-i]
-
+    #### Backward ####
+    u[-2] = q_temp[-1]/b_temp[-1]
+    for i in range(2,n):          # 3*n flops
+        u[-i] = (q_temp[-i] - a*u[-i+1])/b_temp[-i]
+    t1 = time.time()
+    total = t1-t0
+    print('CPU time: %.5g s' % total)
+    print('Grid size:',n)
+    return u
 
 def v(x):       # closed-formed solution, 7 flops
     return 1 - (1-np.exp(-10))*x - np.exp(-10*x)
-t1 = time.time()
-total = t1-t0
-print('CPU time: %.3f s' % total)
 
-plt.plot(x,u)
-plt.plot(x,v(x))
-plt.legend(["Numerical solution","Closed-form solution"])
-plt.title("Grid size: %i, CPU time = %.3f s" % (n,total)); plt.grid()
-plt.show()
+u_10 = spes_algo(1000)
+u_100 = spes_algo(10000)
+u_1000 = spes_algo(100000)
+u_1000 = spes_algo(1000000)
+
+#plt.plot(x,u)
+#plt.plot(x,v(x))
+#plt.legend(["Numerical solution","Closed-form solution"])
+#plt.title("Grid size: %i, CPU time = %.3f s" % (n,total)); plt.grid()
+#plt.show()
+
+print(0.002372 - 0.0018089)
+
+print(0.023866 - 0.018122)
+
+print(0.24264 - 0.18424)
+
+print(2.5207 -1.8112)
+
+
+
+"""
+special
+CPU time: 0.0018089 s
+Grid size: 1000
+CPU time: 0.018122 s
+Grid size: 10000
+CPU time: 0.18424 s
+Grid size: 100000
+CPU time: 1.8112 s
+Grid size: 1000000
+
+"""
